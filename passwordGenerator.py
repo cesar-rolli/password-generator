ccd1a7b5dc0689203dd737997c5c2943397550c2
Cesar Rolli <52143802+cesar-rolli@users.noreply.github.com>
2021-02-06 16:20:59 -0300

    V 1.0.2
    Added QR Code! Improve Architecture and style. Creat a list of passwords instead 1 at time (security)? Create a safe place to save passwords.

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,6 +1,14 @@
 import random
 from tkinter import *
 from tkinter.ttk import *
+import tkinter as tk
+
+from PIL import Image
+import pyqrcode
+
+#Image dimensions (this dimensions is capable to read a 400 characters password).
+widthImage = 150
+lengthImage = 150
 
 root = Tk()
 lengthEntry = IntVar()
@@ -11,9 +19,26 @@ def copyPasswordToClipboard():
   root.clipboard_clear()
   root.clipboard_append(generatedPassword)
 
-def generate():
+def create():
+  #Create the password.
   password1 = passwordGeneration()
   passwordOutput.insert(10, password1)
+
+  #Create the QR Code.
+  qr_data = passwordOutput.get()
+  passwordInQRCode = pyqrcode.create(qr_data)
+  passwordInQRCode.png('passwordInQRCode.png', scale = 4)
+
+  #Resize the QR Code generated.
+  passwordInQRCodeResized = Image.open("passwordInQRCode.png")
+  passwordInQRCodeResized = passwordInQRCodeResized.resize((widthImage,lengthImage),Image.ANTIALIAS)
+  passwordInQRCodeResized.save(fp="passwordInQRCodeResized.png")
+
+  #Show the password in QR Code.
+  qrCodeImage = tk.PhotoImage(file = "passwordInQRCodeResized.png")
+  w = tk.Label(root, image = qrCodeImage)
+  w.qrCodeImage = qrCodeImage
+  w.grid(row = 3, column = 1)
 
 def passwordGeneration():
   passwordOutput.delete(0, END)
@@ -24,43 +49,55 @@ def passwordGeneration():
   numbers = "0123456789"
   password = ""
 
-  #If the type includes all characters
+  #If the type includes all characters.
   if passwordType.get() == 0:
     for i in range(0, length):
       password += random.choice(allCharacters + numbers)
     return password
   
-  #If the type is only numbers
+  #If the type is only numbers.
   if passwordType.get() == 1:
     for i in range(0, length):
       password += random.choice(numbers)
     return password
 
-# # GUI # #
-#Title of GUI window
-root.title("Password Generator")
+#Show "Your password's QR Code will appear here." in QR Code format.
+qrCodeMessageResized = Image.open("qrCodeMessage.png")
+qrCodeMessageResized = qrCodeMessageResized.resize((widthImage, lengthImage),Image.ANTIALIAS)
+qrCodeMessageResized.save(fp="qrCodeMessageResized.png")
+qrCodeImage = tk.PhotoImage(file = "qrCodeMessageResized.png")
+w = tk.Label(root, image = qrCodeImage)
+w.qrCodeImage = qrCodeImage
+w.grid(row = 3, column = 1)
 
-#Create label for length of password
+
+# # GUI # #
+#Title of GUI window.
+root.title("Password Generator")
+# root.geometry('250x150')
+
+#Create label for length of password.
 lengthLabel = Label(root, text = "Length")
 lengthLabel.grid(row = 0, column = 0)
 lengthEntry = Entry(root)
 lengthEntry.grid(row = 0, column = 1)
-generateButton = Button(root, text = "Generate", command = generate)
+generateButton = Button(root, text = "Generate", command = create)
 generateButton.grid(row = 0, column = 2)
 
 copy_buttons = Button(root, text = "Copy", command = copyPasswordToClipboard)
 copy_buttons.grid(row = 1, column = 2)
 
-#Radio buttons to set 
+#Radio buttons to set.
 allCharactersButton = Radiobutton(root, text = "All Characters", variable = passwordType, value =0)
 allCharactersButton.grid(row = 2, column = 0, sticky = "E")
 onlyNumbers = Radiobutton(root, text = "Only Numbers", variable = passwordType, value = 1)
 onlyNumbers.grid(row = 2, column = 1, sticky = "E")
 
-#Create label and entry to show
+#Create label and entry to show.
 passwordLabel = Label(root, text = "Password")
 passwordLabel.grid(row = 1)
 passwordOutput = Entry(root)
 passwordOutput.grid(row = 1, column = 1)
+
 
 root.mainloop()
